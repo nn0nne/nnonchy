b6004b35da1811f58acc017b25b4adaccea8bad5
ananda17gb <ananda.widigdo17@gmail.com>
2026-02-19 23:11:50 +0700

    u

--- .config/nonevim/lua/nnonne/plugins/linter.lua
+++ .config/nonevim/lua/nnonne/plugins/linter.lua
@@ -21,7 +21,6 @@ return {
 			css = { "stylelint" },
 			json = { "jsonlint" },
 			yaml = { "yamllint" },
-			prisma = { "prisma-language-server" },
 			env = { "dotenv-linter" },
 			zig = { "zls" }, -- zls includes diagnostics
 		}
