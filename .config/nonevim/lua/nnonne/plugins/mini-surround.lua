89b6a3879e72ada9c4936daf91ba8f1473ad54a4
ananda17gb <ananda.widigdo17@gmail.com>
2026-03-11 16:39:15 +0700

    u

--- .config/nonevim/lua/nnonne/plugins/mini-surround.lua
+++ .config/nonevim/lua/nnonne/plugins/mini-surround.lua
@@ -1,33 +1,47 @@
 return {
 	"nvim-mini/mini.surround",
+	-- lazy = false,
 	version = "*",
-	keys = {
-		{ "<Space>sa", desc = "Add surrounding" },
-		{ "<Space>sd", desc = "Delete surrounding" },
-		{ "<Space>sf", desc = "Find surrounding" },
-		{ "<Space>sF", desc = "Find surrounding left" },
-		{ "<Space>sh", desc = "Highlight surrounding" },
-		{ "<Space>sr", desc = "Replace surrounding" },
-		{ "<Space>sl", desc = "Suffix last" },
-		{ "<Space>sn", desc = "Suffix next" },
-	},
+	event = { "BufReadPost", "BufNewFile" },
 	config = function()
 		require("mini.surround").setup({
+			-- Add custom surroundings to be used on top of builtin ones. For more
+			-- information with examples, see `:h MiniSurround.config`.
 			custom_surroundings = nil,
+
+			-- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
 			highlight_duration = 500,
+
+			-- Module mappings. Use `''` (empty string) to disable one.
 			mappings = {
-				add = "<Space>sa",
-				delete = "<Space>sd",
-				find = "<Space>sf",
-				find_left = "<Space>sF",
-				highlight = "<Space>sh",
-				replace = "<Space>sr",
-				suffix_last = "<Space>sl",
-				suffix_next = "<Space>sn",
+				add = "<Space>sa", -- Add surrounding in Normal and Visual modes
+				delete = "<Space>sd", -- Delete surrounding
+				find = "<Space>sf", -- Find surrounding (to the right)
+				find_left = "<Space>sF", -- Find surrounding (to the left)
+				highlight = "<Space>sh", -- Highlight surrounding
+				replace = "<Space>sr", -- Replace surrounding
+
+				suffix_last = "<Space>sl", -- Suffix to search with "prev" method
+				suffix_next = "<Space>sn", -- Suffix to search with "next" method
 			},
+
+			-- Number of lines within which surrounding is searched
 			n_lines = 20,
+
+			-- Whether to respect selection type:
+			-- - Place surroundings on separate lines in linewise mode.
+			-- - Place surroundings on each line in blockwise mode.
 			respect_selection_type = false,
+
+			-- How to search for surrounding (first inside current line, then inside
+			-- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+			-- 'cover_or_nearest', 'next', 'prev', 'nearest'. For more details,
+			-- see `:h MiniSurround.config`.
 			search_method = "cover",
+
+			-- Whether to disable showing non-error feedback
+			-- This also affects (purely informational) helper messages shown after
+			-- idle time if user input is required.
 			silent = false,
 		})
 	end,
