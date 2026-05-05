ac4d4418e2861c7ba8583db410dad119bbe1409a
ananda17gb <ananda.widigdo17@gmail.com>
2026-05-05 18:03:43 +0700

    u

--- .config/nonevim/lua/nnonne/plugins/mini/tabline.lua
+++ .config/nonevim/lua/nnonne/plugins/mini/tabline.lua
@@ -8,6 +8,12 @@ function M.setup()
 	require("mini.tabline").setup({
 		show_icons = true,
 		format = function(buf_id, label)
+			local is_active = buf_id == vim.api.nvim_get_current_buf()
+
+			local prefix = is_active and " 󰝥 " or "   "
+
+			local default = require("mini.tabline").default_format(buf_id, label)
+
 			local errors = #vim.diagnostic.get(buf_id, { severity = vim.diagnostic.severity.ERROR })
 			local warnings = #vim.diagnostic.get(buf_id, { severity = vim.diagnostic.severity.WARN })
 
@@ -19,9 +25,11 @@ function M.setup()
 				diagnostic_suffix = diagnostic_suffix .. "  " .. warnings
 			end
 
-			return require("mini.tabline").default_format(buf_id, label) .. diagnostic_suffix
+			return prefix .. default .. diagnostic_suffix
 		end,
 	})
+
+	vim.api.nvim_set_hl(0, "MiniTablineCurrent", { bg = "#6e94b2", fg = "#141415" })
 end
 
 return M
